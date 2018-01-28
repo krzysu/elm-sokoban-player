b1ce0b67bfeab010d051f31fa295997d03bcf317
Krzysztof Urbas <krzysu@interia.pl>
2018-01-28 14:12:33 +0100

    service worker for offline support

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import './css/main.css';
 import './css/game.css';
 import { Main } from './Main.elm';
+import registerServiceWorker from './js/registerServiceWorker';
 import isTouchDevice from './js/isTouchDevice';
 import { getLevels, storeLevels, getLevelsData, storeLevelsData} from './js/storage';
 
+// init elm app
 const app = Main.embed(document.getElementById('root'), {
     levels: getLevels(),
     levelsData: getLevelsData(),
     isTouchDevice: isTouchDevice(),
 });
 
+// subscribe to ports
 app.ports.portStoreLevels.subscribe(data => {
     storeLevels(data);
 });
@@ -18,7 +21,10 @@ app.ports.portStoreData.subscribe(data => {
     storeLevelsData(data);
 });
 
+// service worker
+registerServiceWorker();
 
+// extra classes
 if (isTouchDevice()) {
     document.body.classList.add('is-touch');
 }
